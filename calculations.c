ac4e66611a72f778bc62ca2ae3c0d33cca9c1941
Dominik <helbing.dominik@gmail.com>
2022-05-02 17:00:41 +0200

    add sign functions for different types

--- calculations.c
+++ calculations.c
@@ -120,3 +120,33 @@ int8_t sign(int32_t nb){
 		return 0;
 	}
 }
+
+
+//-------------------------------------------------------------------------------------------
+
+
+// sign for floating point numbers
+int8_t signf(float nb){
+	if(nb < 0){
+		return -1;
+	}else if(nb > 0){
+		return 1;
+	}else{
+		return 0;
+	}
+}
+
+
+//-------------------------------------------------------------------------------------------
+
+
+// sign for short integers
+int8_t signs(int8_t nb){
+	if(nb < 0){
+		return -1;
+	}else if(nb > 0){
+		return 1;
+	}else{
+		return 0;
+	}
+}
